a01a5c953da98b70b2e6e7e6a0ca1d31662ddfb8
mjarret <mjarret@umd.edu>
2021-05-02 14:02:20 -0400

    fixed some minor issues

--- dyads.py
+++ dyads.py
@@ -17,6 +17,9 @@ class Z2:
 		a1,b1,c = scale(other.a,other.b,other.c,self.c)
 		a,b,c = reduce(a-a1,b-b1,c)						# Reduction step doubles runtime, but maybe helps in unseen ways, can be removed
 		return(Z2(a,b,c))
+
+	def __neg__(self):
+		return Z2(-self.a,-self.b,self.c)
 		
 	def __mul__(self, other):							# Multiplication is straightforard
 		a = self.a*other.a + 2*self.b*other.b
@@ -25,9 +28,16 @@ class Z2:
 		a,b,c = reduce(a,b,c)
 		return(Z2(a,b,c))
 	
+	def __eq__(self, other):
+		if isinstance(other, Z2):
+			return (self.a == other.a and self.b == other.b and self.c == other.c)
+		else if isinstance(other, list):
+			return (self.a == list[0] and self.b == list[1] and self.c == list[2])
+		return False
+	
 	def __str__(self):
 		return '[%g, %g, %g]' % (self.a, self.b, self.c)
-	
+		
 def scale(a,b,c,k):			
 	while c < k: 			# Doing things as a loop works faster than computing how many times to do it
 		b = a
--- equivalence.py
+++ equivalence.py
@@ -3,38 +3,34 @@ import math
 import dyads
 import time
 
+
 # Input: matrices A and B
 # Output: True if Transpose(A).B is a signed permutation matrix and False otherwise
 def areEquivalent(A,B):
 	for row1 in A:
-		sum = 0 	# Sum of absolute values of terms in a row
-		count = 0 	# Count number of non-zero elements		
+		sum = 0 							# Sum of absolute values of terms in a row
 		for row2 in B:
-			dp = dot(row1,row2)				# Dot product of rows
-			if dp !=0: 
-				if dp not in [1,-1]: return False	# Reject anything that isn't in {0,1,-1}
-				count += 1 				# Increase count
-			if count > 1: return False 			# Maximum 1 non-zero element
-	return True 
+			dp = dot(row1,row2)					# Dot product of rows
+			if dp.b != 0 or dp.c != 0: return False		# First check to see if either b or c are non zero, note that this relies on dp being reduced
+			if dp.a not in [0,1,-1]: return False			# If other checks passed, this shouldn't hit, can probably remove with testing
+			sum += a*a
+			if sum > 1: return False 				# Maximum 1 non-zero element
+		if sum != 1: return False					# Failsafe in case of null sum
+	return True
 
 def dot(a,b):
-	if len(a) != len(b):	raise Exception("Vectors of unequal length.")	
+	if len(a) != len(b):	raise Exception("Vectors of unequal length.")		# Removing this check will make things faster and should be guaranteed by data types
 	ret = dyads.Z2(0,0,0)
-	for i in range(len(a)):
-		ret += a[i]*b[i]
+	for i in range(len(a)): ret += a[i]*b[i]					# Sum elementwise products (don't use sum method, because need to generate a list)
 	return ret
-
-def recTime(s):
-	start = time.time()
-	s
-	return time.time()-start
 
 def main():
 	a = dyads.Z2(40,2,6)
 	b = dyads.Z2(3,10,3)
+	c = dyads.Z2(3,10,3)
 	start = time.time()
-	for i in range(100000): a*b
+	for i in range(100000): a-b
 	print(time.time()-start)
-	
+
 if __name__ == "__main__":
 	main()	
