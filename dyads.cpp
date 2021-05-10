32a9e5c7dd0105ff65e40f12de19d30640dbe5fb
mjarret <mjarret@umd.edu>
2021-05-10 13:33:02 -0400

    re-implemented reductions throughout

--- dyads.cpp
+++ dyads.cpp
@@ -37,7 +37,7 @@ class Z2 {
             int k = max(this->K,other.K);
             int* arr0 = scale(k);
             int* arr1 = other.scale(k);  
-            return Z2(arr0[0]+arr1[0],arr0[1]+arr1[1],k);        
+            return Z2(arr0[0]+arr1[0],arr0[1]+arr1[1],k).reduce();        
         }
 
         Z2 & operator+=(Z2 &other) {
@@ -47,6 +47,7 @@ class Z2 {
             this->A = arr0[0] + arr1[0];
             this->B = arr0[1] +arr1[1];
             this->K = k;
+            reduce();
             return *this;
         }
 
@@ -57,7 +58,7 @@ class Z2 {
             int k = max(K,other.K);
             int* arr0 = scale(k);
             int* arr1 = tmp.scale(k);
-            return Z2(arr0[0]-arr1[0],arr0[1]-arr1[1],k);          
+            return Z2(arr0[0]-arr1[0],arr0[1]-arr1[1],k).reduce();          
         }
 
         // Input: a Z2 other
@@ -70,7 +71,6 @@ class Z2 {
             A = other.A;
             B = other.B;
             K = other.K;
-            reduce();
             return *this;
         }
 
@@ -166,7 +166,6 @@ class SO6 {
                         dot_product += next;
                     }
                     // if(dot_product.B != 0 || dot_product.K != 0) return false;
-                    dot_product.reduce();
                     if(dot_product.K != 0) return false;
                     // tot += (dot_product.A)*(dot_product.A);
                     // if(tot > 1) return false;
