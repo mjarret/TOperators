9ede3ef68e714ad3da46bc5ffbeab95ebabf42b2
mjarret <mjarret@umd.edu>
2021-05-29 13:20:01 -0400

    Added sorting, sign fixing, and lexicographic ordering

--- SO6.cpp
+++ SO6.cpp
@@ -5,73 +5,90 @@
 #include "SO6.hpp"
 
 /**
- * Basic constructor. Initializes Zero matrix.
- *
- */
-SO6::SO6(){
-    name = "0";
-    for(int i=0; i<6; i++){
-        for(int j=0; j<6; j++)
-            arr[i][j]=Z2();
-    }
-    genOneNorm();
-}
-
-/**
- * Constructor that initializes zero matrix with arbitrary name
- * @param n the name of the object
- */
-SO6::SO6(std::string n){
-    name = n;
-    //initializes all entries as 0
-    for(int i=0; i<6; i++){
-        for(int j=0; j<6; j++)
-            arr[i][j]=Z2();
-    }
-    genOneNorm();
-}
-
-/**
  * Method to compare two Z2 arrays of length 6 lexicographically
  * @param first array of Z2 of length 6
  * @param second array of Z2 of length 6
  * @return -1 if first < second, 0 if equal, 1 if first > second
  */
-int compareLists(Z2 first[6], Z2 second[6]) {
+bool lexLess (Z2* first, Z2* second) {
     for(int i = 0; i < 6 ; i++) {
-        if(first[i].toFloat() < second[i].toFloat()) return -1;
-        if(first[i].toFloat() > second[i].toFloat()) return 1;
+        if(first[i] < second[i]) return true;
+        if(second[i] < first[i]) return false;
     }
-    return 0;    // At this point they are the same
+    return false;
 }
+
+/**
+ * Basic constructor. Initializes Zero matrix.
+ *
+ */
+SO6::SO6(){
+    for(int i=0; i<6; i++){
+        for(int j=0; j<6; j++)
+            arr[i][j]=Z2();
+    }
+}
+
+// /**
+//  * Constructor that initializes zero matrix with arbitrary name
+//  * @param n the name of the object
+//  */
+// SO6::SO6(std::string n){
+//     name = n;
+//     //initializes all entries as 0
+//     for(int i=0; i<6; i++){
+//         for(int j=0; j<6; j++)
+//             arr[i][j]=Z2();
+//     }
+//     genOneNorm();
+// }
 
 /**
  * Constructor that initializes arbitrary matrix with arbitrary name
  * @param a array of Z2 that the SO6 will take as values
  * @param n the name of the object
  */
-SO6::SO6(Z2 a[6][6], std::string n){
-    name = n;
-    Z2 null = Z2(0,0,0);
+SO6::SO6(Z2 a[6][6]){
     //initializes SO6's entries according to a
-    for(int i = 0; i<6; i++){
-        int sign = 0;
-        int j = 0;
+    for(int col = 0; col<6; col++){
+        short sign = 0;
+        short row = 0;
         while(sign == 0) {
-            if(a[i][j] == null) {
-                arr[i][j] = null;
-                j++;
+            if(a[row][col] == 0) {
+                arr[col][row] = Z2(0,0,0);
+                row++;
                 continue;
             }
-            if(a[i][j].toFloat() > 0) sign = 1;
-            else sign = -1;
-            j++;
+            if(a[row][col] < 0) {
+                sign = -1;
+                arr[col][row] = -a[row][col];
+            }
+            else {
+                sign = 1;
+                arr[col][row] = a[row][col];
+            }
+            row++;
         }
-        while(j<6) {
-            if(sign == 1) arr[i][j] = a[i][j];
-            else arr[i][j]=-a[i][j];
-            j++;
+        while(row<6) {
+            if(sign == 1) arr[col][row] = a[row][col];
+            else arr[col][row]=-a[row][col];
+            row++;
         }
+    }
+
+    for(int row= 0; row < 6; row++) {
+        for(int col = 0; col < 6; col++) {
+            std::cout << arr[col][row].toFloat() << ",";
+        }
+        std::cout << std::endl << std::endl;
+    }
+    sort();
+    std::cout << "***********************************" << std::endl;
+    for(int row= 0; row < 6; row++) {
+        for(int col = 0; col < 6; col++) {
+            std::cout << arr[col][row].toFloat() << ",";
+        }
+        std::cout << std::endl << std::endl;
     }
 }
 
@@ -82,7 +99,7 @@ SO6::SO6(Z2 a[6][6], std::string n){
  */
 SO6 SO6::operator*(SO6& other){
     //multiplies operators assuming COLUMN,ROW indexing
-    SO6 prod(name + other.getName());
+    SO6 prod;
     Z2 next;
     for(int i=0; i<6; i++){
         for(int j = 0; j<6; j++){
@@ -92,64 +109,97 @@ SO6 SO6::operator*(SO6& other){
             }
         }
     }
+    prod.fixSign();
+    prod.sort();
     return prod;
 }
 
-SO6 SO6::sort(SO6& other) {
-    const Z2 nullZ2 = Z2(0,0,0);
-    Z2 list[6]; 
-    SO6 toReturn;
-    for(int i = 0; i<6 ; i++) {
-        list[i] = nullZ2;
-    }
-    for(int i = 0; i<6 ; i++) {
-        if(compareLists(list,arr[i]) <= 0) {
-            for(int j = 0; j < 6; j++) toReturn.arr[i][j] = arr[i][j];
+void SO6::fixSign() {
+    for(int col = 0; col<6; col++){
+        short sign = 0;
+        short row = 0;
+        while(sign == 0) {
+            if(arr[col][row] == Z2(0,0,0)) {
+                row++;
+                continue;
+            }
+            if(arr[col][row] < 0) {
+                sign = -1;
+                arr[col][row] = -arr[col][row];
+            }
+            else {
+                sign = 1;
+                arr[col][row] = arr[col][row];
+            }
+            row++;
         }
-        else {
-            for(int j = 0; j < 6; j++) toReturn.arr[i][j] = -arr[i][j];   
+        while(row<6) {
+            if(sign == 1) arr[col][row] = arr[col][row];
+            else arr[col][row]=-arr[col][row];
+            row++;
         }
     }
-    return toReturn;
 }
 
-/**
- * @brief 
- * Assume we have input as 
- * 
- * @param other 
- * @return short 
- */
-short SO6::lexComp(SO6 &other) {
-    int compRes;
+void SO6::sort() {
+    Z2* myZ2[] = {arr[0],arr[1],arr[2],arr[3],arr[4],arr[5]};
+    std::vector<Z2*> myvector(myZ2,myZ2+6);
+    std::sort (myvector.begin(), myvector.end(),lexLess);
+    Z2 arr2[6][6];
     for(int i = 0; i<6; i++) {
-        compRes = compareLists(arr[i], other.arr[i]);
-        if(compRes != 0) return compRes;
+        for(int j = 0; j<6; j++) {
+            arr2[i][j] = (myvector.at(i))[j];
+        }
     }
-    return 0;                   // They're equal
-} 
+    for(int i = 0; i<6; i++) {
+        for(int j = 0; j<6; j++) {
+            arr[i][j] = arr2[i][j];
+        }
+    }
+}
 
-/** overloads == method to check equality up to signed column permutation
+/** overloads == method to check equality of SO6 matrices
  *  @param other reference to SO6 to be checked against
- *  @return whether or not (*this) and other are equivalent up to signed column permutation
+ *  @return whether or not (*this) and other are equivalent
  */
 bool SO6::operator==(SO6 &other) {
-    bool perms[6] = { false, false, false, false, false, false };
-    bool isMatch;
-    for (int i = 0; i < 6; i++) {
-        isMatch = false;                                                                            // Column i is presently unmatched
-        for (int j = 0; j < 6; j++) {
-            if(perms[j]) continue;                                                                  // If we have already matched this column of s, don't look at it again
-            isMatch = std::equal(std::begin(arr[i]),std::end(arr[i]),std::begin(other.arr[j]));     // Check if the columns are equal
-            if(isMatch) {
-                perms[j]=true;                                                                      // Exclude j in the future
-                break;
-            }
-        }    
-        if(!isMatch) return false;                              //We completed a loop with no match, so return false at this point
+    // SO6 are the same if any triangle is the same
+    // TODO: maybe figure out which triangle (upper,lower,right,left) exits fastest on average
+    //       my guess is that because of sorting, lower triangles should exit faster than upper triangles
+    //       since we sort with the left, it may also be the case that the lower right triangle exists faster than lower left
+    for(int col = 5; col>-1 ; col--) {                                
+        for(int row = 5; row>5-col-1; row--) {                        
+            if(arr[col][row]!=other.arr[col][row]) return false;
+        }
     }
-    return true;                                                // Everything was matched
+    return true;
 }
+
+bool SO6::operator%(SO6 &other) {
+    return false;
+}
+
+// /** overloads == method to check equality up to signed column permutation
+//  *  @param other reference to SO6 to be checked against
+//  *  @return whether or not (*this) and other are equivalent up to signed column permutation
+//  */
+// bool SO6::operator==(SO6 &other) {
+//     bool perms[6] = { false, false, false, false, false, false };
+//     bool isMatch;
+//     for (int i = 0; i < 6; i++) {
+//         isMatch = false;                                                                            // Column i is presently unmatched
+//         for (int j = 0; j < 6; j++) {
+//             if(perms[j]) continue;                                                                  // If we have already matched this column of s, don't look at it again
+//             isMatch = std::equal(std::begin(arr[i]),std::end(arr[i]),std::begin(other.arr[j]));     // Check if the columns are equal
+//             if(isMatch) {
+//                 perms[j]=true;                                                                      // Exclude j in the future
+//                 break;
+//             }
+//         }    
+//         if(!isMatch) return false;                              //We completed a loop with no match, so return false at this point
+//     }
+//     return true;                                                // Everything was matched
+// }
 
 void SO6::genOneNorm()
 {
@@ -160,40 +210,44 @@ void SO6::genOneNorm()
     }
 }
 
-//Returns true if and only if s and o are +/- column permutations of each other
 bool SO6::isPerm(SO6 s) {
-    bool perms[6] = { false, false, false, false, false, false };
-    bool isMatch;
-    for (int i = 0; i < 6; i++) {
-        isMatch = false;                                        // Column i is presently unmatched
-        for (int j = 0; j < 6; j++) {
-            if(perms[j]) continue;                              // If we have already matched this column of s, don't look at it again
-            
-            // Check if they match with the same sign
-            for(int k = 0; k < 6; k++) {
-               if(arr[k][i] != s.arr[k][j]) break;              // We need them to match for every element.  
-               if(k == 5) isMatch = true;                       // Only hit me if everything has matched
-            }
-            if(isMatch) {                                       // If we matched, we can move on at this point
-                perms[j] = true;                                // Exclude j from future consideration
-                break;                                          // Move to next value of i
-            }
-
-            // Check if they match with opposite sign, can be combined with above methods to read easier. This shouldn't hit with the way that things have currently been implemented in the Z2 constructor, but I'm worried about the T matrices or the matrices that result after matrix multiplication. Should also add a method for multiplying a Z2 by an int.
-            for(int k = 0; k <6; k++) {
-                if(-arr[k][i] != s.arr[k][j]) break;
-                if(k == 5) isMatch = true;
-            }
-
-            if(isMatch) {
-                perms[j] = true;
-                break;
-            }
-        }    
-        if(!isMatch) return false;                              //We completed a loop with no match, so return false at this point
-    }
-    return true;                                                // Everything was matched
+    return *this == s;
 }
+
+// //Returns true if and only if s and o are +/- column permutations of each other
+// bool SO6::isPerm(SO6 s) {
+//     bool perms[6] = { false, false, false, false, false, false };
+//     bool isMatch;
+//     for (int i = 0; i < 6; i++) {
+//         isMatch = false;                                        // Column i is presently unmatched
+//         for (int j = 0; j < 6; j++) {
+//             if(perms[j]) continue;                              // If we have already matched this column of s, don't look at it again
+            
+//             // Check if they match with the same sign
+//             for(int k = 0; k < 6; k++) {
+//                if(arr[k][i] != s.arr[k][j]) break;              // We need them to match for every element.  
+//                if(k == 5) isMatch = true;                       // Only hit me if everything has matched
+//             }
+//             if(isMatch) {                                       // If we matched, we can move on at this point
+//                 perms[j] = true;                                // Exclude j from future consideration
+//                 break;                                          // Move to next value of i
+//             }
+
+//             // Check if they match with opposite sign, can be combined with above methods to read easier. This shouldn't hit with the way that things have currently been implemented in the Z2 constructor, but I'm worried about the T matrices or the matrices that result after matrix multiplication. Should also add a method for multiplying a Z2 by an int.
+//             for(int k = 0; k <6; k++) {
+//                 if(-arr[k][i] != s.arr[k][j]) break;
+//                 if(k == 5) isMatch = true;
+//             }
+
+//             if(isMatch) {
+//                 perms[j] = true;
+//                 break;
+//             }
+//         }    
+//         if(!isMatch) return false;                              //We completed a loop with no match, so return false at this point
+//     }
+//     return true;                                                // Everything was matched
+// }
 
 /**
  * Generates LDE. Needs to be called any time the matrix is modified to make sure LDE stays current
@@ -216,10 +270,10 @@ bool SO6::isPerm(SO6 s) {
  * @returns reference ostream with the matrix's display form appended
  */
 std::ostream& operator<<(std::ostream& os, const SO6& m){
-    for(int i = 0; i<6; i++){
+    for(int row = 0; row<6; row++){
         os << '[';
-        for(int j = 0; j<6; j++)
-            os << m(i,j)<<' ';
+        for(int col = 0; col<6; col++)
+            os << m(col,row)<<' ';
         os << "] \n";
     }
     os << "\n";
--- SO6.hpp
+++ SO6.hpp
@@ -2,17 +2,20 @@
 class SO6{
 public:
     SO6();
-    SO6(std::string); //initializes zero matrix
-    SO6(Z2[6][6], std::string); //initializes matrix according to a 6x6 array of Z2
+    // SO6(std::string); //initializes zero matrix
+    // SO6(Z2[6][6], std::string); //initializes matrix according to a 6x6 array of Z2
+    SO6(Z2[6][6]); //initializes matrix according to a 6x6 array of Z2
     SO6 operator*(SO6&); //mutliplication
     // int operator<(SO6&);
     short lexComp(SO6&);
-    SO6 sort(SO6&);
+    void fixSign();
+    void sort();
     inline Z2& operator()(int i, int j){return arr[i][j];} //returns the (i,j)th entry
+    bool operator%(SO6&);
     inline const Z2& operator()(int i, int j) const{return arr[i][j];} //returns the (i,j)th entry but for const
     bool operator==(SO6&); //checking equality up to signed permutation
-    inline std::string getName(){return(name);} //getter for Name
-    inline void setName(std::string newName){name = newName;}
+    // inline std::string getName(){return(name);} //getter for Name
+    // inline void setName(std::string newName){name = newName;}
     //inline int getLDE(){return(LDE);} //getter for LDE
     void genOneNorm(); //Returns 1-norm of the first row
     bool isPerm(SO6 s); //Returns true if and only if s is similar to this object
@@ -21,7 +24,7 @@ public:
     friend std::ostream& operator<<(std::ostream&,const SO6&); //display
 private:
     Z2 arr[6][6];
-    std::string name;
+    // std::string name;
     Z2 norm;
     //int LDE;
 };
